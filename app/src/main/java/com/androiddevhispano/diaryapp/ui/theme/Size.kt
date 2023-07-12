6b1f89297ee20dbfb0bae5aa9635eadfb41b7ce5
Raul Emilio Astete Acuña <rastete.dev@gmail.com>
2023-07-11 19:18:56 -0500

    Add standard Size values

---
+++ app/src/main/java/com/androiddevhispano/diaryapp/ui/theme/Size.kt
@@ -0,0 +1,13 @@
+package com.androiddevhispano.diaryapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Size {
+    val tiny = 1.dp
+    val extraSmall = 2.dp
+    val small = 4.dp
+    val medium = 6.dp
+    val large = 8.0.dp
+    val extraLarge = 12.dp
+    val doubleExtraLarge = 16.dp
+}
