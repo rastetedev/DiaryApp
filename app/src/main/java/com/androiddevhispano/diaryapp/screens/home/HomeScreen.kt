ec22cc9df9721a7fc0c139c06057b49e1ca965d4
Raul Emilio Astete Acuña <rastete.dev@gmail.com>
2023-07-13 16:10:09 -0500

    Add top bar animation to hide when scroll content

--- app/src/main/java/com/androiddevhispano/diaryapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/androiddevhispano/diaryapp/screens/home/HomeScreen.kt
@@ -7,14 +7,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -23,6 +26,7 @@ import com.androiddevhispano.diaryapp.models.Diary
 import com.androiddevhispano.diaryapp.utils.RequestState
 import java.time.LocalDate
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     diariesRequestState: RequestState<Map<LocalDate, List<Diary>>>,
@@ -31,13 +35,17 @@ fun HomeScreen(
     onMenuClicked: () -> Unit,
     onSignOutClicked: () -> Unit
 ) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
     NavigationDrawer(
         drawerState = drawerState,
         onSignOutClicked = onSignOutClicked
     ) {
         Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
                 HomeTopBar(
+                    scrollBehavior = scrollBehavior,
                     onMenuClicked = onMenuClicked
                 )
             },
--- app/src/main/java/com/androiddevhispano/diaryapp/screens/home/HomeTopBar.kt
+++ app/src/main/java/com/androiddevhispano/diaryapp/screens/home/HomeTopBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.androiddevhispano.diaryapp.R
@@ -16,11 +17,13 @@ import com.androiddevhispano.diaryapp.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeTopBar(
+    scrollBehavior: TopAppBarScrollBehavior,
     onMenuClicked: () -> Unit
 ) {
     TopAppBar(
+        scrollBehavior = scrollBehavior,
         navigationIcon = {
-            IconButton(onClick = onMenuClicked ) {
+            IconButton(onClick = onMenuClicked) {
                 Icon(imageVector = Icons.Default.Menu, contentDescription = null)
             }
         },
@@ -28,10 +31,10 @@ fun HomeTopBar(
             Text(text = stringResource(id = R.string.diary))
         },
         actions = {
-            IconButton(onClick = onMenuClicked ) {
+            IconButton(onClick = onMenuClicked) {
                 Icon(imageVector = Icons.Default.DateRange, contentDescription = null)
             }
-            IconButton(onClick = onMenuClicked ) {
+            IconButton(onClick = onMenuClicked) {
                 Icon(imageVector = Icons.Default.Delete, contentDescription = null)
             }
         }
